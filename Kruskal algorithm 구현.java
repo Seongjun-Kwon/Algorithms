c040d849a92c92998841c848501ae52783cc7054
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2021-08-04 02:48:32 +0900

    Update Kruskal algorithm 구현.java

--- Kruskal algorithm 구현.java
+++ Kruskal algorithm 구현.java
@@ -27,7 +27,6 @@ public class Kruskal
 	static int e; // 간선의 수
 	static PriorityQueue<A> pq = new PriorityQueue<A>(); // 간선의 가중치를 최소 힙으로 하는 우선순위 큐
 	static int[] parent; // union-find 에서 필요한 부모 노드를 담는 배열
-	static boolean[] visit;
 	static int result = 0;
 	static int cnt = 0;
 
@@ -57,8 +56,16 @@ public class Kruskal
 		v = Integer.parseInt(st.nextToken());
 		e = Integer.parseInt(st.nextToken());
 		parent = new int[v + 1];
-		visit = new boolean[v + 1];
-		
+
+		for (int i = 0; i < e; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			pq.add(new A(a, b, c));
+		}
+
 		for (int i = 1; i <= v; i++)
 			parent[i] = i;
 
